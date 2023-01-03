b6aefb8d2b8523cc17d5b4d0320a72b9e008ff4c
Jecheon Ryu <rjc1704@gmail.com>
2023-01-03 15:44:36 +0900

    feat: Movie 스크린 UI 과제

--- theme.js
+++ theme.js
@@ -2,7 +2,9 @@ import { GREEN_COLOR, YELLOW_COLOR } from "./colors";
 
 export const lightTheme = {
   title: GREEN_COLOR,
+  upcomingText: "black",
 };
 export const darkTheme = {
   title: YELLOW_COLOR,
+  upcomingText: "white",
 };
